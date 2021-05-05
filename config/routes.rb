771f824487aae086f773d119fb5ef7c498942efa
gianluca perrone <perronegian@hotmail.com>
2021-05-05 12:49:14 +0800

    remove devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   delete "api/v1/notes/bulk", to: "api/v1/notes#bulk_destroy", as: :bulk_destroy
   root to: 'pages#home'
     namespace :api, defaults: { format: :json } do
@@ -8,7 +7,6 @@ Rails.application.routes.draw do
     end
   end
 
-  post "/login", to: "users#login", as: :login
-  get "/login", to: "users#token_authenticate", as: :token_auth
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
