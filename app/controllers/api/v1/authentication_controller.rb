bb231f79693bc6e5280ff0d4a566196f256fac4d
gianluca perrone <perronegian@hotmail.com>
2021-05-21 14:39:03 +0800

    a

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::AuthenticationController < Api::V1::BaseController
   skip_before_action :authenticate_user!, raise: false
 
   def create
+    return unless params[:user]
     user = User.find_by(email: params[:user][:email])
     if user && user.valid_password?(params[:user][:password])
       render json: { token: JsonWebToken.encode(sub: user.id) }
